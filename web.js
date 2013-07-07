d87eef1575a5b9ab63fc7f7a24cf4ff35dbab575
Janos Kelemen <janosk21@gmail.com>
2013-07-07 09:27:06 +0000

    addded Buffer object

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 var buffer = new Buffer(fs.readFileSync('index.html','utf-8'),'utf-8');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toSring());
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
